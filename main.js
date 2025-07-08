f6c30c5441d3d7240b168a4c3c125d147f7c551a
OFabuloso <johrod11@gmail.com>
2025-07-08 17:54:23 -0300

    retifield bug error testimonial

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-// Menu - abre e fecha
-const nav = document.querySelector('#header nav');
-const toggle = document.querySelectorAll('nav .toggle');
+// Menu  - abre e fecha 
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
 
-for (const element of toggle) {
-  element.addEventListener('click', function () {
-    nav.classList.toggle('show');
-  });
+for (const element of toggle){
+  element.addEventListener('click', function(){
+    nav.classList.toggle('show')
+  })
 }
 
-// Cliques no menu aberto
-const links = document.querySelectorAll('nav ul li a');
+// cliques no menu aberto
+const links = document.querySelectorAll('nav ul li a')
 
-for (const link of links) {
-  link.addEventListener('click', function () {
-    nav.classList.remove('show');
-  });
+for (const link of links){
+  link.addEventListener('click', function(){
+    nav.classList.remove('show')
+  })
 }
 
-// Testimonials Slide Carousel
+// Testimonials Slide Caroussel
 const swiper = new Swiper('.testimonials', {
   slidesPerView: 1,
   pagination: {
@@ -26,76 +26,78 @@ const swiper = new Swiper('.testimonials', {
   },
   mousewheel: true,
   keyboard: true,
-  loop: false, // Desativado para evitar comportamentos estranhos
   breakpoints: {
     767: {
       slidesPerView: 2,
       setWrapperSize: true,
-    },
-  },
+    }
+  }
 });
 
 // ScrollReveal
-const scrollReveal = ScrollReveal({
+const scrollReveal = scrollReveal({
   origin: 'top',
   distance: '30px',
   duration: 800,
-  reset: true,
-});
+  reset: true
+})
 
-scrollReveal.reveal(
-  `#home .image, #home .text, #about .image, #about .text, #services header, #services .card, #testimonials header, #testimonials .testimonial, #contact .text, #contact .links`,
-  { interval: 50 }
-);
+scrollReveal.reveal(`
+#home .image, #home .text, #about .image, #about .text, #services header, #services .card, #testimonials header, #testimonials .testimonial, #contact .text, #contact .links,
+footer .brand, footer .social
+`,{interval: 50})
+
+
+
 
 // Animação - Sombra do Header com o scroll
 function changeThemeNavBar() {
-  const header = document.querySelector('#header');
-  const navHeight = header.offsetHeight;
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
 
-  if (window.scrollY >= navHeight) {
-    header.classList.add('scroll');
+  if(window.scrollY >= navHeight){
+    header.classList.add('scroll')
   } else {
-    header.classList.remove('scroll');
-  }
+    header.classList.remove('scroll')
+  } 
 }
 
 // Opção ativa do Menu conforme a página
 const sections = document.querySelectorAll('main section[id]');
 function activateMenuDurationSection() {
+
   const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
 
   for (const section of sections) {
-    const sectionTop = section.offsetTop;
-    const sectionHeight = section.offsetHeight;
-    const sectionId = section.getAttribute('id');
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectoinId = section.getAttribute('id')
 
     const checkpointStart = checkpoint >= sectionTop;
     const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
 
-    const menuItem = document.querySelector(`nav ul li a[href*="${sectionId}"]`);
-    if (checkpointStart && checkpointEnd) {
-      menuItem.classList.add('active');
+    if(checkpointEnd && checkpointStart) {
+      document.querySelector('nav ul li a[href*=' + sectoinId + section[id] + ']').classList.add('active')
     } else {
-      menuItem.classList.remove('active');
+      document.querySelector('nav ul li a[href*=' + sectoinId + section[id] + ']').classList.remove('active')
     }
   }
 }
 
-// Botão volta ao topo
+// Botao volta ao topo, revelação
 function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top');
-  if (backToTopButton) {
-    if (window.scrollY >= 560) {
-      backToTopButton.classList.add('show');
-    } else {
-      backToTopButton.classList.remove('show');
-    }
-  }
+  const backToTopButton = document.querySelector('.back-to-top') 
+
+  if(window.scrollY >= 560){
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  } 
 }
 
-window.addEventListener('scroll', function () {
-  backToTop();
-  changeThemeNavBar();
-  activateMenuDurationSection();
-});
+window.addEventListener('scroll', function() {
+  backToTop(),
+  changeThemeNavBar()
+  activateMenuDurationSection()
+}) 
+
